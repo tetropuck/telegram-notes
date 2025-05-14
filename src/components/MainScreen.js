a61a54a6a769f5e835d8ebac518c4b739458fa4a
tetropuck <poslovsky2008@yandex.ru>
2025-05-15 02:14:35 +0700

    Переработана стрелка, указывающая на кнопку на главном экране. Добавлена анимация

--- src/components/MainScreen.js
+++ src/components/MainScreen.js
@@ -1,13 +1,16 @@
+import ArrowIcon from '../assets/arrow.svg';
 import React from 'react';
 import './MainScreen.css';
 
 const MainScreen = ({ onCreate }) => {
     return (
         <div className="main-screen">
-            <div className="empty-state">
-                <p className="message">Для начала работы создайте свою первую заметку</p>
-                <div className="arrow">↘</div>
-            </div>
+<div className="empty-state">
+  <p className="message">Для начала работы создайте свою первую заметку</p>
+  <div className="arrow">
+    <img src={ArrowIcon} alt="стрелка" className="arrow-icon" />
+  </div>
+</div>
             <button className="create-button" onClick={onCreate}>+</button>
         </div>
     );
